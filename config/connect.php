92bc06c10864ec3a0ae80db5d0178bed04d1623f
kira-ito <84730151+kira-ito@users.noreply.github.com>
2022-08-31 02:32:16 +0900

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,14 +1,8 @@
 <?php
-    // $host = "mysql57.notitle.sakura.ne.jp";
-    // $dbName = "notitle_1";
-    // $user = "notitle";
-    // $password = "Kira0819";
-    // $dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
-
-    $host = "localhost:8889";
+    $host = "localhost:";
     $dbName = "todo";
     $user = "root";
-    $password = "root";
+    $password = "";
     $dsn = "mysql:host={$host};dbname={$dbName};charser=utf8mb4";
 
     $pdo = new PDO($dsn, $user, $password);
