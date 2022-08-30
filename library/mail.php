eb7874bb470a0eaad8d93645bed8c5fd1ecd5753
kira-ito <84730151+kira-ito@users.noreply.github.com>
2022-08-31 03:29:31 +0900

    Update mail.php

--- library/mail.php
+++ library/mail.php
@@ -6,8 +6,6 @@
 
 declare(strict_types=1);
 
-/* vnasrgbkhkcwgeso */
-
 mb_language('Japanese');
 mb_internal_encoding('UTF-8');
 
@@ -23,49 +21,26 @@ function encodeHeader($value)
     );
 }
 
-/**
- * 送信元のメールアドレス。ご自身のものに書き換えてください。
- */
 $from = 'test@gmail.com';
-
-/**
- * 送信先のメールアドレス。送信して問題の無いアドレスに書き換えてください。
- */
-// $to[] = 'kira_neymar@icloud.com';
-// $to[] = 'itokira0819@gmail.com';
-
-/**
- * メールの表題
- */
 $subject = '会員登録ありがとうございます！';
-
 
 function sendmail($to, $name)
 {
-    /**
-     * メールの本文
-     */
     $body = <<< BODY
 {$name}さん！　こんにちは！
 この度はTODOアプリにご登録いただきありがとうございます。
 BODY;
 
-    /**
-     * メールの送信元(日本語表記)
-     */
     $sender = encodeHeader('TODOアプリ管理者');
-    // メールのヘッダ行を生成する。
+    
+    // メールヘッダ行の生成
     $header = <<< HEADER
     From: {$sender} <{gmial.smtp.test@gmail.com}>
     Reply-To: {gmial.smtp.test@gmail.com}
     HEADER;
 
-    // メール送信する。
     $isMailSent = mb_send_mail($to, '会員登録ありがとうございます！', $body, $header);
 
     echo $isMailSent ? 'メールを送信しました。' : 'メールは送信できませんでした。';
     echo PHP_EOL;
 }
-// main($from, $to[0], $subject, "山田太郎");
-// main($from, $to[1], $subject, "田中アサリ");
-// main($from, $to, $subject, "田中アサリ");
